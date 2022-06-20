700a47f96d023755aba8ae64a33e7ff9bb533213
unknown <abhisheksrivastava9719@gmail.com>
2022-06-20 16:49:48 +0530

    Check if two lines are equal

--- Line.cs
+++ Line.cs
@@ -8,9 +8,19 @@ namespace Line_comparasion
 {
     internal class Line
     {
-        public static double length(double x1, double y1, double x2, double y2)
+        public static double length(int num)
         {
+            Console.WriteLine("Enter cordinates for line "+num);
+            Console.WriteLine("Enter x1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
             return (Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)));
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using Line_comparasion;
 Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
-Console.WriteLine("Enter x1");
-double x1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Enter y1");
-double y1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Enter x2");
-double x2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Enter y2");
-double y2 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("length of this line is " +Line.length(x1,y1,x2,y2));
+double l1 = Line.length(1);
+double l2 = Line.length(2);
+if (l1 == l2)
+    Console.WriteLine("Equal Lines");
+else
+    Console.WriteLine("Not Equal Lines");
