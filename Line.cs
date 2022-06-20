408b74c4ba4e431064c656f368b2dd7a8383ea96
unknown <abhisheksrivastava9719@gmail.com>
2022-06-20 16:31:57 +0530

    Length of the line

---
+++ Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_comparasion
+{
+    internal class Line
+    {
+        public static double length(double x1, double y1, double x2, double y2)
+        {
+            return (Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+﻿using Line_comparasion;
+Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+Console.WriteLine("Enter x1");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter y1");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter x2");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter y2");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("length of this line is " +Line.length(x1,y1,x2,y2));
