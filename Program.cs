1cb8638a45f777ff2e7e90a50be3e280c4440b6c
unknown <abhisheksrivastava9719@gmail.com>
2022-06-20 17:28:38 +0530

    Lines compared

--- Program.cs
+++ Program.cs
@@ -4,5 +4,7 @@ double l1 = Line.length(1);
 double l2 = Line.length(2);
 if (l1 == l2)
     Console.WriteLine("Equal Lines");
+else if(l1>l2)
+    Console.WriteLine("Line 1 is greater in length than Line 2");
 else
-    Console.WriteLine("Not Equal Lines");
+    Console.WriteLine("Line 2 is greater in length than Line 1");
